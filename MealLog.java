e4e0db665eb6923d4b2d15b72d94a6ee32cc4e4a
Greg London <gregdlondon@gmail.com>
2017-10-15 17:08:16 -0500

    Created getWhys() method

--- MealLog.java
+++ MealLog.java
@@ -13,16 +13,42 @@ public class MealLog
 		{
 			Class.forName("com.mysql.jdbc.Driver");
 			db = DriverManager.getConnection("jdbc:mysql://localhost:3306/meal_log", "root", "");
+			stmt = db.createStatement();
 		}
 		catch(Exception ex)
 		{
 			System.out.println("DB Error: " + ex);
 		}
+			
+	}
+
+	private void getWhys()
+	{
+		String query = "SELECT why FROM whys ORDER BY why DESC";
+		System.out.println("Reasons to lose weight\n");
+			
+		try
+		{
+			rs = stmt.executeQuery(query);
+			while(rs.next())
+				System.out.println(rs.getString(1));
+
+			rs.close();
+
+		}  
+		catch(Exception ex)
+		{
+			System.out.println("DB Error: " + ex);
+		}
+			
+
+
 	}
 
 	public static void main(String[] args)
 	{
-		
+		MealLog m = new MealLog();
+		m.getWhys();
 		System.exit(0);
 	}
 }
